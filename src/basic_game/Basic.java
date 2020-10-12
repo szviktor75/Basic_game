0362a4455133ba4d3fa2189dcbbd8c37e6c489a9
Szőllősi Viktor <szollosiviktor1975@gmail.com>
2020-10-12 21:09:15 +0200

    Basic game első commit.

---
+++ src/basic_game/Basic.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basic_game;
+
+/**
+ *
+ * @author szollosiviktor
+ */
+public class Basic {
+    private int numb1;
+    private  int numb2;
+    private  int numb3;
+
+    public int getNumb1() {
+        return numb1;
+    }
+
+    public int getNumb2() {
+        return numb2;
+    }
+
+    public int getNumb3() {
+        return numb3;
+    }
+
+    public void setNumb1(int numb1) {
+        this.numb1 = numb1;
+    }
+
+    public void setNumb2(int numb2) {
+        this.numb2 = numb2;
+    }
+
+    public void setNumb3(int numb3) {
+        this.numb3 = numb3;
+    }
+    
+    
+}
